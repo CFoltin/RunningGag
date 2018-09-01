83526d5a65cb0131546b627bc8ae4480d4c4e35e
Christian Foltin <christianfoltin@users.sourceforge.net>
2018-09-01 17:33:15 +0200

    Share and open actions fixed.

--- app/src/main/java/eu/johannes/runninggag/RunResult.java
+++ app/src/main/java/eu/johannes/runninggag/RunResult.java
@@ -1,11 +1,12 @@
 package eu.johannes.runninggag;
-import android.app.Activity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -39,10 +40,14 @@ public class RunResult extends AppCompatActivity {
 
 
     private static final String TAG = RunResult.class.getName();
+    public static final String APPLICATION_GPX_XML = "application/gpx+xml";
     MapView map = null;
     private Intent shareIntent;
+    private OnlyOneRun run;
+    private ShareActionProvider mShareActionProvider;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
@@ -64,17 +69,17 @@ public class RunResult extends AppCompatActivity {
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         Bundle bundle = getIntent().getExtras();
-        OnlyOneRun run = bundle.getParcelable("com.example.runs.run");
+        run = bundle.getParcelable("com.example.runs.run");
 
 
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
         List<GeoPoint> geoPoints = new ArrayList<>();
-        for(DataPoint dataPoint : run.getDataPoints()){
+        for (DataPoint dataPoint : run.getDataPoints()) {
 
-            Log.d("RunResult", String.valueOf(dataPoint));
-            GeoPoint geo = new GeoPoint(dataPoint.getLatitude(),dataPoint.getLongitude());
+            //Log.d("RunResult", String.valueOf(dataPoint));
+            GeoPoint geo = new GeoPoint(dataPoint.getLatitude(), dataPoint.getLongitude());
             geoPoints.add(geo);
         }
 
@@ -93,38 +98,10 @@ public class RunResult extends AppCompatActivity {
             }
         });
         map.getOverlayManager().add(line);
-
-
-        // create share intent:
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-        String gpxFile = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<gpx xmlns=\"http://www.topografix.com/GPX/1/1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"1.1\" creator=\"RunningGag\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\"><trk><trkseg>";
-        for(DataPoint dataPoint : run.getDataPoints()){
-            gpxFile += "<trkpt lat=\"" + dataPoint.getLatitude() + "\" lon=\"" +dataPoint.getLongitude() + "\" ><time>" + df.format(new Date(dataPoint.getTime()))  + "</time> </trkpt>";
-        }
-        gpxFile += "</trkseg></trk></gpx>";
-        shareIntent = new Intent();
-        shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.setType("application/gpx+xml");
-        if (shareIntent.resolveActivity(getPackageManager()) != null) {
-            Uri gpxURI = null;
-            try {
-                File temp=File.createTempFile("RunningGag_",".gpx",getCacheDir());
-                temp.deleteOnExit();
-                FileWriter writer = new FileWriter(temp);
-                writer.write(gpxFile);
-                writer.close();
-                gpxURI = FileProvider.getUriForFile(this,
-                        getString(R.string.file_provider_authority),
-                        temp);
-
-            } catch (IOException ex) {
-                Log.e(TAG, ex.getMessage());
-            }
-            shareIntent.putExtra(Intent.EXTRA_STREAM, gpxURI);
-        }
+        createShareAction();
     }
 
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -133,7 +110,7 @@ public class RunResult extends AppCompatActivity {
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -141,8 +118,6 @@ public class RunResult extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
-
-    private ShareActionProvider mShareActionProvider;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -156,8 +131,76 @@ public class RunResult extends AppCompatActivity {
         mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
         mShareActionProvider.setShareIntent(shareIntent);
 
-        // Return true to display menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_gpx:
+                callGpxAction();
+                break;
+        }
         return true;
     }
 
+    private void createShareAction() {
+        // create gpx intent:
+        String gpxFile = getGpxFileContent();
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType(APPLICATION_GPX_XML);
+        if (shareIntent.resolveActivity(getPackageManager()) != null) {
+            Uri gpxURI = getTemporaryUriForFile(gpxFile);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, gpxURI);
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Log.d(TAG, "Intent:  " + shareIntent + " URI: " + gpxURI);
+        }
+    }
+
+    private void callGpxAction() {
+        // create gpx intent:
+        String gpxFile = getGpxFileContent();
+        Intent gpxIntent = new Intent();
+        gpxIntent.setAction(Intent.ACTION_VIEW);
+        gpxIntent.setType(APPLICATION_GPX_XML);
+        if (gpxIntent.resolveActivity(getPackageManager()) != null) {
+            Uri gpxURI = getTemporaryUriForFile(gpxFile);
+            gpxIntent.setData(gpxURI);
+            gpxIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//            shareIntent.putExtra(Intent.EXTRA_STREAM, gpxURI);
+            Log.d(TAG, "Intent:  " + gpxIntent + " URI: " + gpxURI);
+        }
+        startActivity(Intent.createChooser(gpxIntent, "Wohin mit dem GPX File?"));
+    }
+
+    @Nullable
+    private Uri getTemporaryUriForFile(String gpxFile) {
+        Uri gpxURI = null;
+        try {
+            File temp = File.createTempFile("RunningGag_", ".gpx", getCacheDir());
+            temp.deleteOnExit();
+            FileWriter writer = new FileWriter(temp);
+            writer.write(gpxFile);
+            writer.close();
+            gpxURI = FileProvider.getUriForFile(this,
+                    getString(R.string.file_provider_authority),
+                    temp);
+
+        } catch (IOException ex) {
+            Log.e(TAG, ex.getMessage());
+        }
+        return gpxURI;
+    }
+
+    @NonNull
+    private String getGpxFileContent() {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        String gpxFile = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<gpx xmlns=\"http://www.topografix.com/GPX/1/1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"1.1\" creator=\"RunningGag\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\"><trk><trkseg>";
+        for (DataPoint dataPoint : run.getDataPoints()) {
+            gpxFile += "<trkpt lat=\"" + dataPoint.getLatitude() + "\" lon=\"" + dataPoint.getLongitude() + "\" ><time>" + df.format(new Date(dataPoint.getTime())) + "</time> </trkpt>";
+        }
+        gpxFile += "</trkseg></trk></gpx>";
+        return gpxFile;
+    }
 }
