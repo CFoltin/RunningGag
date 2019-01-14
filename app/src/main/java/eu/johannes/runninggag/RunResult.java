02e3eddfbc34f856cf8ff39b00e3ba1f220491f7
Christian Foltin <christianfoltin@users.sourceforge.net>
2019-01-14 17:46:26 +0100

    Export improved

--- app/src/main/java/eu/johannes/runninggag/RunResult.java
+++ app/src/main/java/eu/johannes/runninggag/RunResult.java
@@ -311,18 +311,22 @@ public class RunResult extends AppCompatActivity {
     }
 
     private Bitmap getBitmapFromView(View view) {
-        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(returnedBitmap);
-        Drawable bgDrawable =view.getBackground();
-        if (bgDrawable!=null) {
-            //has background drawable, then draw it on the canvas
-            bgDrawable.draw(canvas);
-        }   else{
-            //does not have background drawable, then draw white background on the canvas
-            canvas.drawColor(Color.WHITE);
-        }
-        view.draw(canvas);
-        return returnedBitmap;
+        view.setDrawingCacheEnabled(true);
+        return view.getDrawingCache();
+
+//        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(returnedBitmap);
+//
+//        Drawable bgDrawable =view.getBackground();
+//        if (bgDrawable!=null) {
+//            //has background drawable, then draw it on the canvas
+//            bgDrawable.draw(canvas);
+//        }   else{
+//            //does not have background drawable, then draw white background on the canvas
+//            canvas.drawColor(Color.WHITE);
+//        }
+//        view.draw(canvas);
+//        return returnedBitmap;
     }
 
 }
